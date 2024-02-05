43a3154b70b9e8086ad368e98b72b9d13bd0557d
JethroNatividad <natividad.jet@gmail.com>
2024-02-05 08:58:20 +0800

    expand problem

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 // Takes in numbers separated by space, then outputs the even numbers.
-
+// Inputs: numbers separated by space
+// Process: Iterating the numbers, make new list with the even numbers.
+// Output: The even numbers.
 fn main() {
     println!("Hello, world!");
 }
