247aece55e73cd8a1b594fee90419d87f74ec1b4
JethroNatividad <natividad.jet@gmail.com>
2024-02-05 08:59:45 +0800

    write constraints

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 // Inputs: numbers separated by space
 // Process: Iterating the numbers, make new list with the even numbers.
 // Output: The even numbers.
+// Constraints: use vector, dont use builtin filter function
+
 fn main() {
     println!("Hello, world!");
 }
