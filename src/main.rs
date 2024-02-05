b0b921592b71cbf3ef7fa2ce3f16d96d5ce03d00
JethroNatividad <natividad.jet@gmail.com>
2024-02-05 09:05:16 +0800

    write tests

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,25 @@
 // Output: The even numbers.
 // Constraints: use vector, dont use builtin filter function
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_filter_even_numbers() {
+        assert_eq!(
+            filter_even_numbers(vec![1, 2, 3, 4, 5, 6, 7, 8]),
+            vec![2, 4, 6, 8]
+        );
+        assert_eq!(filter_even_numbers(vec![1, 3, 5, 7]), vec![]);
+        assert_eq!(
+            filter_even_numbers(vec![-1, -2, -6, -8, -9, 3]),
+            vec![-2, -6, -8]
+        );
+
+        assert_eq!(filter_even_numbers(vec![0]), vec![0]);
+    }
+}
 fn main() {
     println!("Hello, world!");
 }
